b95cb530eb141c657ec87f10f3c6c3fd5e4e5334
Abubacker A K <abuabdul86@hotmail.com>
2025-08-06 16:19:42 +0100

    Fix the alignment

--- src/components/Mars/WatneyMessage.tsx
+++ src/components/Mars/WatneyMessage.tsx
@@ -17,11 +17,11 @@ export default function WatneyMessage({
         <span className="italic">Final Log Entry</span>
       </h2>
 
-      <p className="text-sm text-zinc-600 dark:text-zinc-300 leading-relaxed italic whitespace-pre-line">
-        {"At some point, everything's gonna go south on you... and you're gonna say, 'This is it. This is how I end.'\n"}
-        {"Now you can either accept that... or you can get to work.\n"}
-        {"You do the math. You solve one problem… and you solve the next one… and then the next.\n"}
-        {"And if you solve enough problems, you get to come home."}
+      <p className="text-sm text-justify text-zinc-600 dark:text-zinc-300 leading-relaxed italic whitespace-pre-line">
+        {"At some point, everything's gonna go south on you... and you're gonna say, 'This is it. This is how I end.' "}
+        {"Now you can either accept that... or you can get to work. "}
+        {"You do the math. You solve one problem… and you solve the next one… and then the next. "}
+        {"And if you solve enough problems, you get to come home. "}
       </p>
 
       <div className="mt-4 text-md text-right text-zinc-500 dark:text-zinc-400 italic">
